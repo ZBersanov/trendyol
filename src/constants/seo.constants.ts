e3fd1af5164fba1abf86979eabbc3b6f65dc445b
zaur bersanov <storeborz@mail.ru>
2025-01-18 19:45:56 +0300

    Инициализация проекта, удаление лишнего и добавление необходимых пакетов и конфигураций

---
+++ src/constants/seo.constants.ts
@@ -0,0 +1,4 @@
+export const NO_INDEX_PAGE = { robots: { index: false, follow: false } }
+
+export const SITE_NAME = 'trendyol'
+export const SITE_DESCRIPTION = 'Короче магаз где можно продавать и покупать'
