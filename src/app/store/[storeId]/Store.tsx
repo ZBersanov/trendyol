2a1aa6a8352e3986dbc120fcea6f09472ee39178
zaur bersanov <storeborz@mail.ru>
2025-01-20 21:10:01 +0300

    Рефакторинг структуры компонентов магазина: удаление старых файлов и добавление новых компонентов Store и StoreIdPage с соответствующими метаданными и макетом

--- src/app/store/[storeId]/Store.tsx
+++ src/app/store/[storeId]/Store.tsx
@@ -1,3 +1,3 @@
 export default function Store() {
-	return <div></div>
+	return <div>Store</div>
 }
--- src/app/store/[storeId]/page.tsx
+++ src/app/store/[storeId]/page.tsx
@@ -4,11 +4,11 @@ import { NO_INDEX_PAGE } from '@/constants/seo.constants'
 
 import Store from './Store'
 
-export const metadata: Metadata = {
+const metadata: Metadata = {
 	title: 'Управление магазином',
 	...NO_INDEX_PAGE
 }
 
-export default function Page() {
+export default function StoreIdPage() {
 	return <Store />
 }
