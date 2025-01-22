a05e46e57035b318895b8aa1ef73833ed0c1b1a0
zaur bersanov <storeborz@mail.ru>
2025-01-22 17:38:29 +0300

    Добавление хуков useDeleteStore и useUpdateStore для управления удалением и обновлением магазина с обработкой успешных и ошибочных состояний

---
+++ src/hooks/queries/stores/useDeleteStore.ts
@@ -0,0 +1,26 @@
+import { useMutation } from '@tanstack/react-query'
+import { useParams, useRouter } from 'next/navigation'
+import { useMemo } from 'react'
+import toast from 'react-hot-toast'
+
+import { PUBLIC_URL } from '@/config/url.config'
+
+import { storeService } from '@/services/store.service'
+
+export function useDeleteStore() {
+	const params = useParams<{ storeId: string }>()
+	const router = useRouter()
+
+	const { mutate: deleteStore, isPending } = useMutation({
+		mutationKey: ['delete store'],
+		mutationFn: () => storeService.delete(params.storeId),
+		onSuccess() {
+			toast.success('Магазин успешно удален')
+			router.push(PUBLIC_URL.home())
+		},
+		onError() {
+			toast.error('Ошибка при удалении магазина')
+		}
+	})
+	return useMemo(() => ({ deleteStore, isPending }), [deleteStore, isPending])
+}
---
+++ src/hooks/queries/stores/useUpdateStore.ts
@@ -0,0 +1,39 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import { useParams } from 'next/navigation'
+import { useMemo } from 'react'
+import toast from 'react-hot-toast'
+
+import { storeService } from '@/services/store.service'
+
+import { IEditStore } from '@/shared/types/store.interface'
+
+export function useUpdateStore() {
+	const params = useParams<{ storeId: string }>()
+
+	const queryClient = useQueryClient()
+
+	const { data: store } = useQuery({
+		queryKey: ['store', params.storeId],
+		queryFn: () => storeService.getById(params.storeId)
+	})
+
+	const { mutate: updateStore, isPending } = useMutation({
+		mutationKey: ['update store'],
+		mutationFn: (data: IEditStore) =>
+			storeService.update(params.storeId, data),
+		onSuccess() {
+			queryClient.invalidateQueries({
+				queryKey: ['profile']
+			})
+			toast.success('Магазин успешно обновлен')
+		},
+		onError() {
+			toast.error('Ошибка при обновлении магазина')
+		}
+	})
+
+	return useMemo(
+		() => ({ store, updateStore, isPending }),
+		[store, updateStore, isPending]
+	)
+}
