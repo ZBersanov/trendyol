fca633ef92bccb630198bb6d38ba5d3bec10c7ff
zaur bersanov <storeborz@mail.ru>
2025-01-28 18:16:10 +0300

    Добавление страницы продукта: реализация компонента для отображения информации о продукте, галереи изображений и отзывов с поддержкой получения похожих товаров

---
+++ src/app/(root)/product/[id]/product-info/ProductInfo.tsx
@@ -0,0 +1,9 @@
+import { IProduct } from '@/shared/types/product.interface'
+
+interface ProductInfoProps {
+	product: IProduct
+}
+
+export default function ProductInfo({ product }: ProductInfoProps) {
+	return <div></div>
+}
