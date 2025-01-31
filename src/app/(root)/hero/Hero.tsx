f712c1121053cc777284016078ef505bdd45d1f2
zaur bersanov <storeborz@mail.ru>
2025-01-31 17:31:41 +0300

    Обновление текста в компоненте Hero и улучшение описания сайта: изменение приветствия на "Добро пожаловать в Trendyol" и уточнение описания платформы.

--- src/app/(root)/hero/Hero.tsx
+++ src/app/(root)/hero/Hero.tsx
@@ -13,7 +13,7 @@ export default function Hero() {
 	return (
 		<div className={styles.section}>
 			<h1 className={styles.heading}>
-				<span>Мохьмад</span> болх меттиг кеч елахь
+				Добро пожаловать в <span>Trendyol</span>
 			</h1>
 			<p className={styles.description}>{SITE_DESCRIPTION}</p>
 			<Link href={PUBLIC_URL.explorer()}>
--- src/constants/seo.constants.ts
+++ src/constants/seo.constants.ts
@@ -1,4 +1,5 @@
 export const NO_INDEX_PAGE = { robots: { index: false, follow: false } }
 
 export const SITE_NAME = 'Trendyol'
-export const SITE_DESCRIPTION = 'Короче магаз где можно продавать и покупать'
+export const SITE_DESCRIPTION =
+	'Платформа Trendyol для покупок любых товаров и создания собственного магазина с CMS'
