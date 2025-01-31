a48c04740af3d7ca26e9ab95ec5bc848abeef2b7
zaur bersanov <storeborz@mail.ru>
2025-01-31 03:26:30 +0300

    Добавление компонента LastUsers: реализация отображения списка покупателей с аватарами, именами и электронной почтой, а также обработка случая отсутствия данных.

--- src/app/store/[storeId]/statistics/middle-statistics/LastUsers.tsx
+++ src/app/store/[storeId]/statistics/middle-statistics/LastUsers.tsx
@@ -1,3 +1,7 @@
+import Image from 'next/image'
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
 import { ILastUsers } from '@/shared/types/statistics.interface'
 
 interface LastUsersProps {
@@ -5,5 +9,38 @@ interface LastUsersProps {
 }
 
 export function LastUsers({ data }: LastUsersProps) {
-	return <div>Overview</div>
+	console.log(data)
+
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Покупатели</CardTitle>
+			</CardHeader>
+			<CardContent>
+				{data.length ? (
+					data.map(item => (
+						<div className='flex items-center justify-between'>
+							<div className='flex'>
+								<Image
+									src={item.avatar}
+									alt={item.name}
+									width={45}
+									height={45}
+									className='rounded-full mr-4'
+								/>
+								<div className='flex flex-col'>
+									<div>{item.name}</div>
+									<div>{item.email}</div>
+								</div>
+							</div>
+
+							<div>+{item.total}</div>
+						</div>
+					))
+				) : (
+					<div>Нет покупателей</div>
+				)}
+			</CardContent>
+		</Card>
+	)
 }
